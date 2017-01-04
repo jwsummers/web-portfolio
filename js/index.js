f1ae09a5eb4d5b53730b786d84691267f89d5166
Jason Summers <jwsummers.dev@gmail.com>
2017-01-03 20:00:11 -0500

    Web Portfolio
    Web Portfolio for freecodecamp challenge project.

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$("about").click(function(){
+$('html, body').animate({ scrollTop: $("about").offset().top},2000);
+});
+
+$("portfolio").click(function(){
+$('html, body').animate({ scrollTop: $("portfolio").offset().top},2000);
+});
+
+$("contact").click(function(){
+$('html, body').animate({ scrollTop: $("contact").offset().top},2000);
+});
+
+$("home").click(function(){
+$('html, body').animate({ scrollTop: $("top").offset().top},2000);
+});
